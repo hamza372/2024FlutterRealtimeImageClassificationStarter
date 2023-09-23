c56950a0885f9f13bbefe451f14d10cd15aef93d
hamza372 <31725222+hamza372@users.noreply.github.com>
2023-09-23 20:54:05 +0500

    2024

---
+++ android/app/src/main/kotlin/com/example/realtime_flutter_image_classification/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.realtime_flutter_image_classification
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,99 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:flutter/services.dart';
+
+
+late List<CameraDescription> _cameras;
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  _cameras = await availableCameras();
+  runApp(const CameraScreen());
+}
+
+class CameraScreen extends StatefulWidget {
+  const CameraScreen({Key? key}) : super(key: key);
+  @override
+  State<CameraScreen> createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {
+  late CameraController controller;
+  CameraImage? img;
+  bool isBusy = false;
+  String result = "results to be shown here";
+
+  //TODO declare ImageLabeler
+
+
+  @override
+  void initState() {
+    super.initState();
+    //TODO initialize labeler
+
+
+    //TODO initialize the controller
+    controller = CameraController(_cameras[0], ResolutionPreset.high);
+    controller.initialize().then((_) {
+      if (!mounted) {
+        return;
+      }
+      controller.startImageStream((image) => {
+        if (!isBusy) {isBusy = true, img = image, doImageLabeling()}
+      });
+      setState(() {});
+    }).catchError((Object e) {
+      if (e is CameraException) {
+        switch (e.code) {
+          case 'CameraAccessDenied':
+            print('User denied camera access.');
+            break;
+          default:
+            print('Handle other errors.');
+            break;
+        }
+      }
+    });
+  }
+
+  doImageLabeling() async {
+    result = "";
+    setState(() {
+      result;
+      isBusy = false;
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (!controller.value.isInitialized) {
+      return Container();
+    }
+    return MaterialApp(
+      home: Stack(
+        fit: StackFit.expand,
+        children: [
+          CameraPreview(controller),
+          Container(
+            margin: const EdgeInsets.only(left: 10, bottom: 10),
+            child: Align(
+              alignment: Alignment.bottomLeft,
+              child: Text(
+                result,
+                style: const TextStyle(color: Colors.white, fontSize: 25),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
